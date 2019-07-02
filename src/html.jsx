fe69f2af1a9df8e8b7cb0bdd7efaea80e693d881
Harris Thompson <harris@harrisjt.com>
2019-07-02 15:43:57 -0500

    fix: gatsby v2 removes need to manually inline styles

--- src/html.jsx
+++ src/html.jsx
@@ -1,16 +1,6 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 
-let stylesStr;
-if (process.env.NODE_ENV === `production`) {
-  try {
-    // eslint-disable-next-line global-require
-    stylesStr = require(`!raw-loader!../public/styles.css`);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
 export default class HTML extends PureComponent {
   static propTypes = {
     body: PropTypes.node.isRequired,
@@ -20,11 +10,6 @@ export default class HTML extends PureComponent {
 
   render() {
     const {body, headComponents, postBodyComponents} = this.props;
-
-    let css;
-    if (process.env.NODE_ENV === `production`) {
-      css = <style id="gatsby-inlined-css" dangerouslySetInnerHTML={{__html: stylesStr}} />;
-    }
 
     return (
       <html lang="en">
@@ -39,7 +24,6 @@ export default class HTML extends PureComponent {
 
           <meta name="apple-mobile-web-app-title" content="harrisjt" />
           <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
-          {css}
         </head>
         <body>
           <div dangerouslySetInnerHTML={{__html: body}} id="___gatsby" />
